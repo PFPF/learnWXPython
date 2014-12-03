208233738fb2adf1b0da73b265eb53bb9a0cad6f
PF <p_fire@126.com>
2014-12-03 09:20:46 -0500

    Lesson2 with happiness -- without checking for happiness=0.

--- Lesson2-Buttons.py
+++ Lesson2-Buttons.py
@@ -8,30 +8,40 @@ import wx
 
 
 #This function is an event handler. It will run when a certain even occurs.
+hap = 10
 def OnClickMe(e):
-	print "Yay! You clicked it."
+	global hap
+	hap += 1
+	print("Happiness: {}".format(hap))
+def OnTimer(e):
+	global hap
+	hap -= 2
+	print("Happiness: {}".format(hap))
 
 #----Main Program Below-----
 
-#In wx we always sart by creating a wx.App. I like to call it "app".
+#In wx we always start by creating a wx.App. I like to call it "app".
 app = wx.App(False)
 
 #Create a new frame.
-frame = wx.Frame(None, wx.ID_ANY, "Josh's Title")
+frame = wx.Frame(None, wx.ID_ANY, "Happy Title")
 
 #Create a new panel
 #A panel is not a whole frame, it is just a smaller collection of things inside the frame.
 panel = wx.Panel(frame)
 
 #Create a button, and put it in my panel
-btnClickMe = wx.Button(panel, label="Click Me", pos=(20,20), size=(200,20))
+btnClickMe = wx.Button(panel, label="Click Me to Please Me", pos=(240,180), size=(200,200))
 
 #Make the button do something!
-#btnClickMe.Bind(wx.EVT_BUTTON, OnClickMe)
+btnClickMe.Bind(wx.EVT_BUTTON, OnClickMe)
+btnClickMe.timer = wx.Timer(btnClickMe, wx.ID_ANY)
+btnClickMe.Bind(wx.EVT_TIMER, OnTimer)
+btnClickMe.timer.Start(1000)
 
 #Show the frame
 frame.Show()
-
+print("Happiness: 10")
 #Make the app listen for clicks, and other events
 app.MainLoop()
 
