2c51206a0b4af6959707b6c732153b1ede712a48
PF <p_fire@126.com>
2014-12-05 09:45:54 -0500

    Lesson 6

--- Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
+++ Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
@@ -1,26 +1,41 @@
 #! /usr/bin/evn python
 
-#import the graphical library
-#import the time module
-
+import wx
+import time
+from random import randint
+times = 0
 class TenButtonFrame(wx.Frame):
-	def __init__(self, parent):
-		wx.Frame.__init__(self, parent, wx.ID_ANY, "Title Here")
+	def __init__(self, parent = None):
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Ten Button Race!!!", size=(600,600))
 		
-		#Make a new Panel
+		self.panel = wx.Panel(self)
 		
-		#Make the start button
-		#Make the other ten buttons
+		self.start = wx.Button(self.panel, label="START!", pos=(250,250))
+		self.btn = wx.Button(self.panel, pos=(randint(1,500), randint(1,500)), size=(randint(10,50),randint(10,50)))
 		#Hide the other ten buttons
-		
+		self.btn.Bind(wx.EVT_CHAR_HOOK, self.OnCheat)
+		self.start.Bind(wx.EVT_BUTTON, self.OnStart)
+		self.btn.Bind(wx.EVT_BUTTON, self.OnBTN)
+		self.btn.Show(False)
 		#Bind all the buttons to their event handlers
 		
 	# Event handler for the start button
+	def OnCheat(self, e):
+		self.btn.Show(False)
+		wx.StaticText(self.panel, label = "You've cheated by pressing the keyboard. Game Over.")
 	def OnStart(self, e):
-		#Make the start button disappear
-		self.startTime = time.time()
+		self.start.Show(False)
+		self.btn.Show(True)
+		time.clock()
 		#Make Button One appear
-		
+	def OnBTN(self, e):
+		global times
+		times += 1
+		if(times == 10):
+			wx.StaticText(self.panel, label = "Your time spent: {}s.".format(time.clock()))
+			self.btn.Show(False)
+		self.btn.SetPosition((randint(1,500), randint(1,500)))
+		self.btn.SetSize((randint(10,50),randint(10,50)))
 	#Other event handlers here
 	
 	#Remember the last event handler needs to print the final time.
