ce64909e9dc941b4f62c6773ab1a93184af36ae3
PF <p_fire@126.com>
2014-12-07 16:44:02 -0500

    Lesson6

--- Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
+++ Lesson6-TenButtonRace-Exercise/tenButtonRaceSkeleton.py
@@ -2,44 +2,113 @@
 
 import wx
 import time
-from random import randint
-times = 0
+from random import randint, choice
+
+btnNum = 0
+btnRemaining = 0
+events = {wx.EVT_BUTTON:"left", wx.EVT_RIGHT_UP:"right"} # after realized that some people don't have middle button
+
 class TenButtonFrame(wx.Frame):
 	def __init__(self, parent = None):
-		wx.Frame.__init__(self, parent, wx.ID_ANY, "Ten Button Race!!!", size=(600,600))
+		wx.Frame.__init__(self, parent, wx.ID_ANY, "Ten Button Race!!!", size=(600,600)) # initialize the frame
 		
-		self.panel = wx.Panel(self)
+		self.panel = wx.Panel(self) # create a panel
+		self.question = wx.StaticText(self.panel, label="How many buttons do you want? Default is 10.\n\nN<=0 means as many as you can", pos=(40, 10))
+		self.inputBox = wx.TextCtrl(self.panel, pos=(200, 80)) # an input-box
 		
-		self.start = wx.Button(self.panel, label="START!", pos=(250,250))
-		self.btn = wx.Button(self.panel, pos=(randint(1,500), randint(1,500)), size=(randint(10,50),randint(10,50)))
-		#Hide the other ten buttons
-		self.btn.Bind(wx.EVT_CHAR_HOOK, self.OnCheat)
-		self.start.Bind(wx.EVT_BUTTON, self.OnStart)
-		self.btn.Bind(wx.EVT_BUTTON, self.OnBTN)
-		self.btn.Show(False)
-		#Bind all the buttons to their event handlers
+		self.start = wx.Button(self.panel, label="START!", pos=(250,250)) # the start button
+		self.btn = wx.Button(self.panel, pos=(randint(1,450), randint(1,450)), size=(randint(10,50),randint(10,50))) # create the button
+		self.prompt = wx.StaticText(self.panel, label="Your next click should be: left button") # Show the next click
+		self.start.Bind(wx.EVT_BUTTON, self.OnStart) # bind the start button's event
+		self.btn.Bind(wx.EVT_BUTTON, self.OnBTN) # bind the racing button's event
+		self.btn.Bind(wx.EVT_CHAR_HOOK, self.OnCheat) # see if the user cheats by pressing keys
+		
+		self.btn.Show(False) # hide the racing button 
+		self.prompt.Show(False) # hide the prompt
 		
 	# Event handler for the start button
-	def OnCheat(self, e):
-		self.btn.Show(False)
-		wx.StaticText(self.panel, label = "You've cheated by pressing the keyboard. Game Over.")
+
 	def OnStart(self, e):
-		self.start.Show(False)
-		self.btn.Show(True)
-		time.clock()
-		#Make Button One appear
+		global btnRemaining, btnNum
+		try:
+			N = int(self.inputBox.GetValue()) # get the information from the user
+			if(N > 0):
+				btnRemaining = N; btnNum = N
+			else:
+				btnRemaining = 0
+		except:
+			if(len(self.inputBox.GetValue()) == 0): btnRemaining = btnNum = 10
+			else: raise ValueError("I can't understand!!\n")
+		
+		self.inputBox.Show(False)
+		self.question.Show(False)
+		self.start.Show(False) # hide the widgets before
+		self.btn.Show(True) # show the button1
+		self.prompt.Show(True) # show the prompt
+		time.clock() # the first call
+	
+	def OnCheat(self, e): # keyboard-cheat
+		#self.btn.Show(False) # so the user can't play
+		#wx.StaticText(self.panel, label = "You've cheated by pressing the keyboard. Game Over.")
+		pass
+	def OnWrong(self, e): # wrong button
+		self.btn.Show(False) # so the user can't play
+		wx.StaticText(self.panel, label = "You've clicked on the wrong mouse button :(")
+		
+		if(btnRemaining <= 0): # endless mode!
+			btnNum = 1 - btnRemaining # calculate the buttons clicked by 0 - btnRemaining + 1
+			spent = time.clock() # second call
+			self.prompt.Show(False) # hide the prompt
+			self.btn.Show(False) # Hide the button
+			
+			try:
+				readF = open("endless.txt", 'r')
+				recordNum = int(readF.readline())
+				recordAvgTime = float(readF.readline()) # read the current record
+				readF.close()
+			except:
+				recordNum = 0; recordAvgTime = 1000000 # no record yet lol
+			recordNum = str(max(recordNum, btnNum)) # compare the record to the buttons clicked
+			recordAvgTime = str(min(recordAvgTime, spent / btnNum)) # compare the record to the avg time
+			
+			writeF = open("endless.txt", 'w')
+			writeF.write(recordNum + '\n' + recordAvgTime) # write the new record to the file.
+			writeF.close()
+			wx.StaticText(self.panel, label = "You've clicked {} buttons.\nYour time spent: {}s.\nYour record is {} buttons and {}s per button.".format(btnNum, spent, recordNum, recordAvgTime))
+			
+			
+				
 	def OnBTN(self, e):
-		global times
-		times += 1
-		if(times == 10):
-			wx.StaticText(self.panel, label = "Your time spent: {}s.".format(time.clock()))
-			self.btn.Show(False)
-		self.btn.SetPosition((randint(1,500), randint(1,500)))
-		self.btn.SetSize((randint(10,50),randint(10,50)))
-	#Other event handlers here
-	
-	#Remember the last event handler needs to print the final time.
-	
+		global btnRemaining, events
+		self.btn.SetPosition((randint(1,500), randint(1,500))) # randomly change the position,
+		self.btn.SetSize((randint(10,50),randint(10,50))) # and size of the button
+		
+		curEvent = choice(events.keys()) # get the next button
+		self.prompt.SetLabel("Your next click should be: {} button".format(events[curEvent]))
+		for event in events:
+			if(event == curEvent):
+				self.btn.Bind(event, self.OnBTN)
+			else:
+				self.btn.Bind(event, self.OnWrong) # wrong
+		
+		btnRemaining -= 1 # minus 1 from it
+		if(btnRemaining == 0): # game over
+			spent = time.clock() # second call
+			self.prompt.Show(False) # hide the prompt
+			self.btn.Show(False) # Hide the button
+			
+			try:
+				readF = open("{}.txt".format(btnNum), 'r')
+				record = float(readF.read()) # read the current record
+				readF.close()
+			except:
+				record = 1000000 # no record yet lol
+			record = str(min(record, spent)) # compare the record to the spent time
+			
+			writeF = open("{}.txt".format(btnNum), 'w')
+			writeF.write(record) # write the new record to the file.
+			writeF.close()
+			wx.StaticText(self.panel, label = "You've clicked {} buttons.\nYour time spent: {}s.\nYour record is {}s.".format(btnNum, spent, record))
 	
 # -------- Main Program Below ------------
 
